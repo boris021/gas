aac7eead550dc0fa4dcc0fd9d608e8ddb7aea905
boris <boris021@mail.ru>
2022-09-06 16:33:07 +0500

    home

--- dist/js/app.js
+++ dist/js/app.js
@@ -5625,15 +5625,17 @@
             if ("auto" === params.slidesPerView && !params.loopedSlides) params.loopedSlides = slides.length;
             swiper.loopedSlides = Math.ceil(parseFloat(params.loopedSlides || params.slidesPerView, 10));
             swiper.loopedSlides += params.loopAdditionalSlides;
-            if (swiper.loopedSlides > slides.length) swiper.loopedSlides = slides.length;
+            if (swiper.loopedSlides > slides.length && swiper.params.loopedSlidesLimit) swiper.loopedSlides = slides.length;
             const prependSlides = [];
             const appendSlides = [];
             slides.each(((el, index) => {
-                const slide = dom(el);
-                if (index < swiper.loopedSlides) appendSlides.push(el);
-                if (index < slides.length && index >= slides.length - swiper.loopedSlides) prependSlides.push(el);
-                slide.attr("data-swiper-slide-index", index);
+                dom(el).attr("data-swiper-slide-index", index);
             }));
+            for (let i = 0; i < swiper.loopedSlides; i += 1) {
+                const index = i - Math.floor(i / slides.length) * slides.length;
+                appendSlides.push(slides.eq(index)[0]);
+                prependSlides.unshift(slides.eq(slides.length - index - 1)[0]);
+            }
             for (let i = 0; i < appendSlides.length; i += 1) $selector.append(dom(appendSlides[i].cloneNode(true)).addClass(params.slideDuplicateClass));
             for (let i = prependSlides.length - 1; i >= 0; i -= 1) $selector.prepend(dom(prependSlides[i].cloneNode(true)).addClass(params.slideDuplicateClass));
         }
@@ -6310,6 +6312,7 @@
             loop: false,
             loopAdditionalSlides: 0,
             loopedSlides: null,
+            loopedSlidesLimit: true,
             loopFillGroupWithBlank: false,
             loopPreventsSlide: true,
             rewind: false,
